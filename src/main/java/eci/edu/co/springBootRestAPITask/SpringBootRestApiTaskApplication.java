20753c43ac1136858fba81102410da4ed4013d75
Carlos A <54123771+Carlos96999@users.noreply.github.com>
2021-08-22 11:55:40 -0500

    Parte 2 del laboratorio finalizado

---
+++ src/main/java/eci/edu/co/springBootRestAPITask/SpringBootRestApiTaskApplication.java
@@ -0,0 +1,13 @@
+package eci.edu.co.springBootRestAPITask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eci/edu/co/springBootRestAPITask/SpringBootRestApiTaskApplicationTests.java
@@ -0,0 +1,7 @@
+package eci.edu.co.springBootRestAPITask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiTaskApplicationTests {}
